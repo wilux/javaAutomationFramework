6af492956c112555094589af60afc53c59116807
floresnes <nestor.flores@storicard.com>
2024-04-30 14:14:42 -0300

    add examples

--- src/test/java/tests/AppTest.java
+++ src/test/java/tests/AppTest.java
@@ -1,10 +1,17 @@
 package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 import static org.testng.Assert.assertTrue;
 
 public class AppTest {
@@ -15,12 +22,36 @@ public class AppTest {
         driver = new FirefoxDriver();
     }
     @Test
-    void myTest() {
+    void myValidTest() {
 
         driver.get("https://www.google.com");
-        
-//        String url = driver.getCurrentUrl();
-//        assertTrue(url.contains("google"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement searchBar = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
+
+        searchBar.sendKeys("pepe");
+        searchBar.submit();
+
+        WebElement searchResults = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("search")));
+
+        String pageText = searchResults.getText();
+        assertTrue(pageText.toLowerCase().contains("pepe"));
+
+    }
+
+    @Test
+    void myInvalidTest() {
+
+        driver.get("https://www.google.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement searchBar = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
+
+        searchBar.sendKeys("pepe");
+        searchBar.submit();
+
+        WebElement searchResults = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("search")));
+
+        String pageText = searchResults.getText();
+        assertTrue(pageText.toLowerCase().contains("nestor"));
 
     }
     @AfterTest
