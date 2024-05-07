769b251d4ab11b3e4b16594b0b8c7674a48a30ca
floresnes <nestor.flores@storicard.com>
2024-05-07 17:01:34 -0300

    add browsers

--- src/test/java/steps/Environment.java
+++ src/test/java/steps/Environment.java
@@ -2,6 +2,7 @@ package steps;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -25,8 +26,16 @@ public class Environment {
     }
 
     @AfterStep
+    public void afterStep(Scenario scenario) {
+//        if (scenario.isFailed()) {
+//            screenshot();
+//        }
+        screenshot();
+    }
+
+
     @Attachment(value = "Screenshot", type = "image/png")
-    public byte[] screenshot() {
+    public byte[] screenshot( ) {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
