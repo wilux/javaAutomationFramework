10634e0f320558375b7eacd088f6b3148d217bd0
floresnes <nestor.flores@storicard.com>
2024-05-07 15:22:09 -0300

    add browsers

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,5 +3,5 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue = "tests.steps", features = "src/test/resources/features")
+@CucumberOptions(glue = "tests.steps", features = "src/test/resources/features", strict = true)
 public class TestRunner {}
